b8b8da81bbe5474ba48daf2a146dff767cbd8c5e
ppiont <piontek0@gmail.com>
2020-07-14 19:55:57 +0200

    small steps

--- create_features.py
+++ create_features.py
@@ -11,6 +11,8 @@ import pandas as pd
 import os
 import rasterio
 import rasterio.plot
+import rasterio.features
+import rasterio.warp
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -31,7 +33,11 @@ raster_extent = np.asarray(raster.bounds)[[0,2,1,3]]
 plt.imshow(raster.read(1), cmap='hot', extent=raster_extent)
 
 # convert band to numpy array
-nparray_test = np.asarray(raster.read(1))
+raster_array = np.asarray(raster.read(1))
+
+
+
+
 
 # load targets
 df = pd.read_csv("germany_targets.csv", index_col = 0).reset_index()
@@ -43,13 +49,55 @@ gdf = gpd.GeoDataFrame(df, geometry = gpd.points_from_xy(df.GPS_LONG, df.GPS_LAT
 # raster.crs
 gdf = gdf.to_crs(raster.crs.data)
 
-gdf.head()
+
+
+coord = (gdf.geometry[i].x, gdf.geometry[i].y)
+    
+
+import rasterio as rio
+
+infile = r"germany_covars/CLM_CHE_BIO02.tif"
+outfile = r'test_{}.tif'
+coordinates = [(x,y) for x, y in zip(gdf.GPS_LONG, gdf.GPS_LAT)]
+
+# NxN window
+N = 3
+
+# Open the raster
+with rio.open(infile) as dataset:
+
+    # Loop through list of coords
+    for i, (lon, lat) in enumerate(len(coordinates)):
+
+        # Get pixel coordinates from map coordinates
+        py, px = dataset.index(lon, lat)
+        print('Pixel Y, X coords: {}, {}'.format(py, px))
+
+        # Build an NxN window
+        window = rio.windows.Window(px - N//2, py - N//2, N, N)
+        print(window)
+
+        # Read the data in the window
+        # clip is a nbands * N * N numpy array
+        clip = dataset.read(window=window)
+
+        # Write out a new file
+        meta = dataset.meta
+        meta['width'], meta['height'] = N, N
+        meta['transform'] = rio.windows.transform(window, dataset.transform)
+
+        with rio.open(outfile.format(i), 'w', **meta) as dst:
+            dst.write(clip)
 
 
 
 
 
 
+# ENVELOPE BUFFER
+# gdf2 = gdf.copy()
+# gdf2.geometry = gdf.geometry.buffer(5000).envelope
+# gdf2.plot()
 
 
 # fig, ax = plt.subplots(figsize=(15, 15))
