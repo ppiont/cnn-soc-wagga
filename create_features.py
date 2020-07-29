77300c430e3d7321d0e00abae781f1d1c5588649
ppiont <piontek0@gmail.com>
2020-07-29 20:27:00 +0200

    ..

--- create_features.py
+++ create_features.py
@@ -6,6 +6,20 @@ Created on Sun May 17 14:41:37 2020
 @author: peter
 """
 
+
+
+
+
+
+import rasterio as rio
+import os
+
+bands = rio.open("data/germany_covars/CLM_CHE_BIO02.tif")
+
+
+band = bands.read(1)
+band.sum()
+
 import geopandas as gpd
 import pandas as pd
 import os
@@ -28,41 +42,47 @@ crs_raster = rio.open("germany_covars/CLM_CHE_BIO02.tif")
 # Load targets
 df = pd.read_csv("germany_targets.csv", index_col = 0)
 gdf = gpd.GeoDataFrame(df, geometry = gpd.points_from_xy(df.GPS_LONG, df.GPS_LAT), crs = "EPSG:4326")
-# Set crs to same as rasters
+# Set crs to same as the raster's
 gdf = gdf.to_crs(crs_raster.crs.data)
 
+#Create list of filenames for cov rasters
 file_list = glob.glob('germany_covars/*.tif')
+#Create list of coordinates for each target
 coordinates = ((x,y) for x, y in zip(gdf.GPS_LONG, gdf.GPS_LAT))
 
 # for each coord(target), get surrounding from each covar raster, then merge to 415 band stack and output with 
 
 
-
 def stack_target_bands(file_list, target_coords, outfile = r"stack_{}.tif", n_win_size = 3):
-
+    #For each target coord
     for i, (lon, lat) in enumerate(target_coords, start = 1):
         
         #Get pixel coordinates from the first raster only
-        with rio.open(file_list[0]) as src0:
+        with rio.open(file_list[0], 'r') as src0:
             py, px = src0.index(lon, lat)
+            print(py, px)
             meta = src0.meta
             #Update meta to reflect the number of layers
             meta.update(count = len(file_list), dtype = rio.uint16)
-        
+            print(meta)
+            
         #Create window
         window = rio.windows.Window(px - n_win_size//2, py - n_win_size//2, n_win_size, n_win_size)
+        print(window)
         
-        #Clip window around each target layer and write bands to stack
+        #Clip window around target for each layer and write bands to stack
         for id, file in enumerate(file_list, start = 1):
-            with rio.open(file) as src1:
+            with rio.open(file, 'r') as src1:
                 #Read the data in the window
                 #clip is a nbands * N * N numpy array
                 clip = src1.read(window = window)
-                
                 #Add transform to metadata
                 meta = src1.meta
                 meta['width'], meta['height'] = n_win_size, n_win_size
                 meta['transform'] = rio.windows.transform(window, src1.transform)
+                meta.update(count = len(file_list), dtype = rio.uint16)
+                
+                print(np.shape(clip)) # something is wrong wit´h the clip shape!
 
                 #Search filename without path and extension
                 band_name = re.search('^(.*)\/(.*)(\..*)$', file).group(2)
@@ -156,13 +176,6 @@ with rio.open('stack.tif', 'w', **meta) as dst:
 #         with rio.open(outfile.format(i), 'w', **meta) as dst:
 #             dst.write(clip)
 ##############################################################################
-
-
-
-
-
-
-
 
 
 
